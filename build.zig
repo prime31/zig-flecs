cba2b29ef7f8cad582cc3736a11eb97ba9600074
Mike <mike@prime31.com>
2022-02-13 10:12:51 -0800

    add raw flecs example

--- build.zig
+++ build.zig
@@ -12,6 +12,7 @@ pub fn build(b: *std.build.Builder) anyerror!void {
 
     const examples = [_][2][]const u8{
         [_][]const u8{ "simple", "examples/simple.zig" },
+        [_][]const u8{ "raw", "examples/raw.zig" },
         [_][]const u8{ "benchmark", "examples/benchmark.zig" },
         [_][]const u8{ "generator", "examples/generator.zig" },
     };
