e7c7d2a232bc85260061817dec0bcd83d068687e
foxnne <49629865+foxnne@users.noreply.github.com>
2022-02-25 20:51:31 -0600

    cleanup and add hierarchies query example

--- examples/entities/hierarchy.zig
+++ examples/entities/hierarchy.zig
@@ -5,7 +5,6 @@ const Position = struct { x: f32, y: f32 };
 const Star = struct {};
 const Planet = struct {};
 const Moon = struct {};
-const ChildOf = struct{};
 
 // TODO: Handle this with Term instead of this.
 const Iterator = struct {
@@ -38,7 +37,7 @@ fn iterateTree(world: flecs.World, e: flecs.Entity, p_parent: Position) void {
         // Iterate children recursively
 
         // TODO: Here is where we want to set the term id to the pair id
-        var it: Iterator = .{ .iter = flecs.c.ecs_term_iter(world.world, &std.mem.zeroInit(flecs.c.ecs_term_t, .{ .id = flecs.c.ECS_PAIR | (flecs.meta.componentId(world.world, ChildOf) << @as(c_int, 32)) + @intCast(u32, e.id) })) };
+        var it: Iterator = .{ .iter = flecs.c.ecs_term_iter(world.world, &std.mem.zeroInit(flecs.c.ecs_term_t, .{ .id = flecs.c.ECS_PAIR | (flecs.c.EcsChildOf << @as(c_int, 32)) + @intCast(u32, e.id) })) };
 
         while (it.next()) {
             iterateTree(world, .{ .world = world.world, .id = it.iter.entities[it.index - 1] }, p_actual);
@@ -55,32 +54,34 @@ pub fn main() !void {
     // Hierarchies use ECS relations and the builtin flecs::ChildOf relation to
     // create entities as children of other entities.
 
+    const child_of = flecs.Entity.init(world.world, flecs.c.EcsChildOf);
+
     const sun = world.newEntityWithName("Sun");
     sun.add(Star);
     sun.set(&Position{ .x = 1, .y = 1 });
 
     const mercury = world.newEntityWithName("Mercury");
-    mercury.addPair(ChildOf, sun);
+    mercury.addPair(child_of, sun);
     mercury.add(Planet);
     mercury.set(&Position{ .x = 1, .y = 1 });
 
     const venus = world.newEntityWithName("Venus");
-    venus.addPair(ChildOf, sun);
+    venus.addPair(child_of, sun);
     venus.add(Planet);
     venus.set(&Position{ .x = 2, .y = 2 });
 
     const earth = world.newEntityWithName("Earth");
-    earth.addPair(ChildOf, sun);
+    earth.addPair(child_of, sun);
     earth.add(Planet);
     earth.set(&Position{ .x = 3, .y = 3 });
 
     const moon = world.newEntityWithName("Moon");
-    moon.addPair(ChildOf, earth);
+    moon.addPair(child_of, earth);
     moon.add(Moon);
     moon.set(&Position{ .x = 0.1, .y = 0.1 });
 
     // Is the Moon a child of Earth?
-    if (moon.hasPair(ChildOf, earth))
+    if (moon.hasPair(child_of, earth))
         std.log.debug("Moon is a child of Earth!", .{});
 
     // Do a depth-first walk of the tree
