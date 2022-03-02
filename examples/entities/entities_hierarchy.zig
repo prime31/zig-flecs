4166018ad3d3548830c48c4673dd1722bfd5bf68
Mike <mike@prime31.com>
2022-03-01 20:51:26 -0800

    use flecs.c.EcsChildOf

--- examples/entities/entities_hierarchy.zig
+++ examples/entities/entities_hierarchy.zig
@@ -32,34 +32,32 @@ pub fn main() !void {
 
     // Create a simple hierarchy.
     // Hierarchies use ECS relations and the builtin flecs::ChildOf relation to create entities as children of other entities.
-    const child_of = flecs.Entity.init(world.world, flecs.c.EcsChildOf);
-
     const sun = world.newEntityWithName("Sun");
     sun.add(Star);
     sun.set(&Position{ .x = 1, .y = 1 });
 
     const mercury = world.newEntityWithName("Mercury");
-    mercury.addPair(child_of, sun);
+    mercury.addPair(flecs.c.EcsChildOf, sun);
     mercury.add(Planet);
     mercury.set(&Position{ .x = 1, .y = 1 });
 
     const venus = world.newEntityWithName("Venus");
-    venus.addPair(child_of, sun);
+    venus.addPair(flecs.c.EcsChildOf, sun);
     venus.add(Planet);
     venus.set(&Position{ .x = 2, .y = 2 });
 
     const earth = world.newEntityWithName("Earth");
-    earth.addPair(child_of, sun);
+    earth.addPair(flecs.c.EcsChildOf, sun);
     earth.add(Planet);
     earth.set(&Position{ .x = 3, .y = 3 });
 
     const moon = world.newEntityWithName("Moon");
-    moon.addPair(child_of, earth);
+    moon.addPair(flecs.c.EcsChildOf, earth);
     moon.add(Moon);
     moon.set(&Position{ .x = 0.1, .y = 0.1 });
 
     // Is the Moon a child of Earth?
-    if (moon.hasPair(child_of, earth))
+    if (moon.hasPair(flecs.c.EcsChildOf, earth))
         std.log.debug("Moon is a child of Earth!", .{});
 
     // Do a depth-first walk of the tree
