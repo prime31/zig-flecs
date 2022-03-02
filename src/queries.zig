674badc1c0d7b0e3881517a97d3cbc3f472acf0f
Mike <mike@prime31.com>
2022-03-01 20:07:46 -0800

    NoMatch modifier

--- src/queries.zig
+++ src/queries.zig
@@ -64,6 +64,15 @@ fn NotFrom(comptime T1: type, comptime T2: type) type {
     };
 }
 
+/// Non matching term. T should not be present in the query unless it should also match for some reason.
+/// Same thing as `Mark(EcsNothing)`.
+pub fn NoMatch(comptime T: type) type {
+    return struct {
+        pub const mask: u8 = flecs.c.EcsNothing;
+        term_type: T,
+    };
+}
+
 pub fn Mask(comptime T: type, mask: u8) type {
     return struct {
         pub const mask: u8 = mask;
